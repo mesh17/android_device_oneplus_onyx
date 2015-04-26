a0990a9631e12df7cc9d5c771ed0dc448aebf656
Justin Driggers <justin@justindriggers.com>
2015-04-26 02:20:30 -0400

    Fix bluetooth

--- device.mk
+++ device.mk
@@ -83,6 +83,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+	javax.btobex
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8974
