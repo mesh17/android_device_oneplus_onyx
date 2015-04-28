07ea88af030d9df6afd68d4caf70c00f08d141f6
Justin Driggers <justin@justindriggers.com>
2015-04-28 01:00:27 -0400

    Fix wireless tethering

--- device.mk
+++ device.mk
@@ -222,10 +222,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
     hostapd \
-    hostapd_default.conf \
     libwpa_client \
     wcnss_service \
     wpa_supplicant
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
